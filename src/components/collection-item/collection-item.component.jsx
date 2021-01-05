4c6288a777cf2eb1ae1c2a5274fce8363392e702
David Beltran <tiqtok@gmail.com>
2021-01-05 13:58:47 -0800

    updating our app with a shop page, collection preview component and collection item component

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './collection-item.styles.scss';
+
+const CollectionItem = ({id, name, price, imageUrl}) => {
+    return (
+        <div key={id} className='collection-item'>
+            <div className='image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+            />
+            
+            <div className='collection-footer'>
+                <span>{name}</span>
+                <span>{price}</span>
+            </div>
+            
+        </div>
+    )
+}
+
+export default CollectionItem;
