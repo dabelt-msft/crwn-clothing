1a57a2e59f4c310a454f428c895afa0bd5c3326d
David Beltran <tiqtok@gmail.com>
2021-01-06 13:27:33 -0800

    user ref empty

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,7 +12,18 @@ const config = {
     measurementId: "G-Y0BDSV2NBT"
   };
 
-  firebase.initializeApp(config);
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+      if (!userAuth) return;
+
+      const userRef = firestore.doc('users/1123u123h1kl2j');
+        
+      const snapShot = await userRef.get();
+
+      console.log(snapShot);
+  } 
+
+//   firebase.initializeApp(config);
+  !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
